4bf4070eddb8ad4ed3617b7d588b861b8ce0718d
viraj vanker... <natvarchampa1950@gmail.com>
2024-12-07 11:43:10 +0530

    Add files via upload

---
+++ C_Cpp/area and paremeter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,b,a;
+	cout<<"Enter the value of length ";
+	cin>>l;
+	cout<<"Enter the value of breath ";
+	cin>>b;
+	
+	cout<<"The area of square is "<<l*l<<endl;
+    
+	cout<<"The paremeter of square is "<<l*4<<endl;
+	
+	cout<<"The area of rectangle is "<<l*b<<endl;
+	
+	cout<<"The paremeter of rectangle is "<<2*(l+b)<<endl; 
+}
